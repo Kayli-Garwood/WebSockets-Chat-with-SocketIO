641bacab13f2424a5612269fbe2a8f7b2b3d06a1
Kayli Garwood <kgarwood@uncc.edu>
2021-11-04 14:52:56 -0400

    Finished Assignment

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ io.on('connection', (socket) => {
     });
 });
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
